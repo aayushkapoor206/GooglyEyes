f8eb4e4f906f5f738adda9639f4fbfc2e28147f6
Aayush Kapoor <aayushkapoor206@gmail.com>
2015-10-09 14:02:42 +0530

    Initial Commit

---
+++ GooglyEyes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GooglyEyes
+//
+//  Created by Aayush Kapoor on 09/10/15.
+//  Copyright © 2015 Aayush Kapoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
