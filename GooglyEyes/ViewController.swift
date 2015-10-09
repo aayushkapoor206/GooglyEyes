aaab8fc9b7b9f985913214cb7f4850531db20283
Aayush Kapoor <aayushkapoor206@gmail.com>
2015-10-09 14:16:19 +0530

    Add Views and Code for Video Input to Photo Output

--- GooglyEyes/ViewController.swift
+++ GooglyEyes/ViewController.swift
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var captureSession: AVCaptureSession?
+    var stillImageOutput: AVCaptureStillImageOutput?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    @IBOutlet weak var capturedImage: UIImageView!
+    @IBOutlet weak var previewVideo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        captureSession = AVCaptureSession()
+        captureSession?.sessionPreset = AVCaptureSessionPresetPhoto
+        
+        let backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        let input = try! AVCaptureDeviceInput(device: backCamera)
+        
+        if (captureSession?.canAddInput(input) != nil) {
+            captureSession?.addInput(input)
+        }
+        
+        stillImageOutput = AVCaptureStillImageOutput()
+        
+        stillImageOutput?.outputSettings = [AVVideoCodecKey: AVVideoCodecJPEG]
+        
+        captureSession?.addOutput(stillImageOutput)
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewVideo.layer.addSublayer(previewLayer!)
+        previewLayer?.frame = previewVideo.bounds
+        
+        captureSession?.startRunning()
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func didPressTakePhoto(sender: UIButton) {
+        
+        if let videoConnection = stillImageOutput?.connectionWithMediaType(AVMediaTypeVideo) {
+            stillImageOutput?.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: {(sampleBuffer, error) in
+                let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+                let dataProvider = CGDataProviderCreateWithCFData(imageData)
+                let cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true, CGColorRenderingIntent.RenderingIntentDefault)
+                let image = UIImage(CGImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.Right)
+                self.capturedImage.image = image
+            })
+        }
+        
+    }
+    
+    @IBAction func didPressGoogleEyes(sender: UIButton) {
+        
+    }
+    
 }
-
